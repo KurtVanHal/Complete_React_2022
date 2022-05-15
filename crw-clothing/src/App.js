479f13730c8c37e5bf38a97810d76337576923ac
Kurt Van Hal <kurt_van_hal@hotmail.com>
2022-05-15 03:39:25 +0200

    Added firebase, google signin authentication and firestore functionality

--- crw-clothing/src/App.js
+++ crw-clothing/src/App.js
@@ -1,7 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-
-import { Routes, Route } from "react-router-dom";
+import SignIn from "./routes/signin/signin.component";
 
 const App = () => {
   const Shop = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
